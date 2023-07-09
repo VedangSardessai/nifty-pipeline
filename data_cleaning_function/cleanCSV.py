d55c061635caefbaa6c20b834f03f4583b8acae0
VedangSardessai <vms112000@gmail.com>
2023-07-10 00:55:37 +0530

    code: changed the clean_transform_data function to round the prices without decimal points

--- data_cleaning_function/cleanCSV.py
+++ data_cleaning_function/cleanCSV.py
@@ -8,7 +8,13 @@ def clean_transform_data(input_file, output_file):
     # Drop any rows with missing values
     cleaned_data = raw_data.dropna()
 
-    # Perform data transformations and data_cleaning_function operations as needed
+    # Round the 'Open', 'High', 'Low', and 'Close' columns to integers (without decimal places)
+    cleaned_data['Open'] = cleaned_data['Open'].round(decimals=0).astype(int)
+    cleaned_data['High'] = cleaned_data['High'].round(decimals=0).astype(int)
+    cleaned_data['Low'] = cleaned_data['Low'].round(decimals=0).astype(int)
+    cleaned_data['Close'] = cleaned_data['Close'].round(decimals=0).astype(int)
+
+    # Perform additional data transformations and cleaning operations as needed
     # Example: Convert data types, handle missing values, normalize data, perform currency conversion, etc.
 
     # Save the cleaned and transformed data to a new CSV file

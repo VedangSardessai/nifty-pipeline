604303ad0b02440cd7d1998d3526f612865dd5db
VedangSardessai <vms112000@gmail.com>
2023-07-09 20:54:06 +0530

    code: changed the code to remove the 'Adj Close' column and add the date in dd-mm-yyyy format in the csv

--- fetch_save_functions/save_data_function/save_data.py
+++ fetch_save_functions/save_data_function/save_data.py
@@ -6,7 +6,14 @@ def save_data_to_csv(data):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     output_file = f"historicalData/nifty_data_{timestamp}.csv"
     cleaned_output_file = f"cleanedHistoricalData/cleaned_nifty_data_{timestamp}.csv"
-    data.to_csv(output_file, index=False)
+
+    data_reset_index = data.reset_index()
+    data_reset_index['Date'] = data_reset_index['Date'].dt.strftime('%d-%m-%Y')
+    data_reset_index.to_csv(output_file, index=False)
+
+    data_reset_index.drop('Adj Close', axis=1, inplace=True)
+    data_reset_index.to_csv(output_file, index=False)
+
     clean_transform_data(output_file, cleaned_output_file)
     print("Data saved to", output_file)
 

ed3e171d7f79a31d6b9e04b1a5ff8c56aab7a73b
VedangSardessai <vms112000@gmail.com>
2023-07-10 19:59:38 +0530

    code: changed the code that wasn't including today

--- Data_Sourcing_and_Extraction/fetch_save_functions/fetch_data_function/fetch_nifty.py
+++ Data_Sourcing_and_Extraction/fetch_save_functions/fetch_data_function/fetch_nifty.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import yfinance as yf
 
 
 def fetch_nifty_data():
     ticker = "^NSEI"  # Ticker symbol for NIFTY index
     start_date = '2007-09-17'
-    end_date = datetime.now().strftime("%Y-%m-%d")
+    end_date = (timedelta(days=1) + datetime.now()).strftime("%Y-%m-%d")
     data = yf.download(ticker, start=start_date, end=end_date)
     return data

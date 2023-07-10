13a4f1d94eaa3c20761f92e1668e70411de2a1ef
VedangSardessai <vms112000@gmail.com>
2023-07-10 12:06:25 +0530

    directories : added extra layer of directories for better understanding

--- Data Sourcing and Extraction/fetch_save_functions/save_data_function/save_data.py
+++ Data Sourcing and Extraction/fetch_save_functions/save_data_function/save_data.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from data_cleaning_function.cleanCSV import clean_transform_data
+from custom_ETL.load_data_postgreSQL import load_csv_to_postgres
 
 
 def save_data_to_csv(data):
@@ -8,12 +9,14 @@ def save_data_to_csv(data):
     cleaned_output_file = f"cleanedHistoricalData/cleaned_nifty_data_{timestamp}.csv"
 
     data_reset_index = data.reset_index()
-    data_reset_index['Date'] = data_reset_index['Date'].dt.strftime('%d-%m-%Y')
+    data_reset_index['Date'] = data_reset_index['Date'].dt.strftime('%Y-%m-%d')
     data_reset_index.to_csv(output_file, index=False)
 
     data_reset_index.drop('Adj Close', axis=1, inplace=True)
     data_reset_index.to_csv(output_file, index=False)
 
     clean_transform_data(output_file, cleaned_output_file)
-    print("Data saved to", output_file)
+    print("Data saved to", cleaned_output_file)
+
+    load_csv_to_postgres(cleaned_output_file)
 

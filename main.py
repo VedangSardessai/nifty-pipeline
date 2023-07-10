1d10e86f140383b5992e782eb516eea290525921
VedangSardessai <vms112000@gmail.com>
2023-07-10 20:02:18 +0530

    code: db connection boilerplate to close connection

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import time
 from Data_Sourcing_and_Extraction.fetch_save_functions.fetch_data_function.fetch_nifty import fetch_nifty_data
 from Data_Sourcing_and_Extraction.fetch_save_functions.save_data_function.save_data import save_data_to_csv
+from db.db_init import conn
 
 while True:
     try:
         nifty_data = fetch_nifty_data()
         save_data_to_csv(nifty_data)
+        conn.close()
         time.sleep(86500)  # Fetch data every 60 seconds
 
     except Exception as e:
